dabda583c2e2d7b232e09a1aaad63e5cad871fb9
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-08-23 20:43:59 +0100

    Automatic configuration with django-heroku

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import django_heroku
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
